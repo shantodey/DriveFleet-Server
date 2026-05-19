c0cc23a102403e659d242be8cc2a79973d2a2dd3
shantodey <ratul3330@gmail.com>
2026-05-19 16:04:09 +0600

    Created API to add car data on the database Nah

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const dns = require("node:dns");
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
-const express =require('express');
-const dotenv= require('dotenv')
+const express = require('express');
+const dotenv = require('dotenv')
+const cors=require("cors");
+
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
 dotenv.config()
 const uri = process.env.MONGODB_URI
 
 const app = express()
-const PORT= process.env.PORT;
+const PORT = process.env.PORT;
+app.use(cors())
+app.use(express.json())
 
-app.get('/',(req,res)=>{
-    res.send("server is runnig fine")
+
+app.get('/', (req, res) => {
+  res.send("server is runnig fine")
 })
 
 
@@ -25,19 +31,38 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+    // creating a Database to stor the data 
+    const db = client.db("DriveFleet")
+
+
+    // Making different collections on the database to store different data
+    const addCarCollection = db.collection("cars")
+
+
+    // sending add card data to database
+    app.post('/addCar', async  (req, res) => {
+      const cars = req.body;
+      const result = await addCarCollection.insertOne(cars)
+      res.json(result)
+    })
+
+
+
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-app.listen(PORT,()=>{
-    console.log(`server is running on port${PORT}`);
-    
+app.listen(PORT, () => {
+  console.log(`server is running on port${PORT}`);
+
 })
