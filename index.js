3e8eb42e3551999853cf90b7f406b013e5acf30d
shantodey <ratul3330@gmail.com>
2026-05-21 21:11:30 +0600

    Comment out MongoDB connection and ping commands for debugging purposes

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const verifyToken = async (req, res, next) => {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     // creating a Database to stor the data 
     const db = client.db("DriveFleet")
 
@@ -149,7 +149,7 @@ async function run() {
       }
     });
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
 
